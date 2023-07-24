be0809c9a8ad114fa3b34831a65b91fb54fa66bb
hagrasy <134404345+hagrasy@users.noreply.github.com>
2023-07-24 19:04:31 +0300

    creat notes app

--- note_app/lib/main.dart
+++ note_app/lib/main.dart
@@ -1,38 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NotesApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Login());
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      body: Text("Hagrasy"),
-    );
-  }
-}
-
-class Login extends StatelessWidget {
-  const Login({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      body: Text("Login"),
-    );
+    return const Placeholder();
   }
 }

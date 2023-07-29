a12459f84f6e693ad244f08a4a19d778b6bc0fc6
hagrasy <134404345+hagrasy@users.noreply.github.com>
2023-07-29 17:42:49 +0300

    Create Edit Note View

---
+++ note_app/lib/views/edit_note_view.dart
@@ -0,0 +1,13 @@
+import 'package:era/views/widgets/edit_view_body.dart';
+import 'package:flutter/material.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: EditNoteViewBody(),
+    );
+  }
+}
--- note_app/lib/views/widgets/note_item.dart
+++ note_app/lib/views/widgets/note_item.dart
@@ -1,3 +1,4 @@
+import 'package:era/views/edit_note_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -6,47 +7,57 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter Tips",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 20, bottom: 24),
-              child: Text(
-                "Build your career with AMIT, they have the most professional team ever.",
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return const EditNoteView();
+          }),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter Tips",
                 style: TextStyle(
-                  color: Colors.black.withOpacity(.5),
-                  fontSize: 20,
+                  color: Colors.black,
+                  fontSize: 26,
                 ),
               ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 24),
+                child: Text(
+                  "Build your career with AMIT, they have the most professional team ever.",
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.5),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  )),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text('Jul25 , 2023',
-                style: TextStyle(color: Colors.black.withOpacity(.6))),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text('Jul25 , 2023',
+                  style: TextStyle(color: Colors.black.withOpacity(.6))),
+            ),
+          ],
+        ),
       ),
     );
   }
